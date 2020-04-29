90f0192412d25dd79d09b76d86eca48eca062321
Manu <manukris@vt.edu>
2020-04-29 19:05:45 -0400

    Added seasonality

--- 18.DLM_FFBS.r
+++ 18.DLM_FFBS.r
@@ -15,7 +15,8 @@
 
 # By M. A. R. Ferreira (April 2012, updated April 2015).
 
-
+require(dlm)
+require(MCMCpack)
 theta.simulation <- function(Tbig,y,sigma2,W,a1,R1)
 { 
   # Define the auxiliary vectors:
@@ -74,87 +75,134 @@ W.simulation <- function(Tbig,y,theta,sigma2,n,ns)
   W <- 1.0 / rgamma(1,a/2,b/2)
   W
 }
+require(MCMCpack)
+drawIGpost <- function(x, a=0.001, b=0.001) {
+  return(rinvgamma(1, a+length(x)/2, b+sum(x^2)/2))
+}
+
 
 # Initializing the parameters, latent and observation vectors.
-sigma2 <- 10
+sigma2 <- 40
+sigmat2 <- 20
+sigmad2 <- 0.2
 W <- 5
 Tbig <- 200
-theta <- rep(0,Tbig)
+theta_org <- rep(0,Tbig)
+delta <- rep(0,Tbig)
 y <- rep(0,Tbig)
 
 # Simulating the data
-theta[1] <- rnorm(1,0,sqrt(W*sigma2))
+theta_org[1] <- rnorm(1,0,sqrt(sigmat2))
+delta[1] <- rnorm(1,0,sqrt(sigmad2))
 y[1] <- theta[1] + rnorm(1,0,sqrt(sigma2))
 for(t in 2:Tbig)
 {
-  theta[t] <- theta[t-1] + rnorm(1,0,sqrt(W*sigma2))
-  y[t] <- theta[t] + rnorm(1,0,sqrt(sigma2))
+  theta_org[t] <- theta_org[t-1] + delta[t-1]+rnorm(1,0,sqrt(sigmat2))
+  delta[t] <- delta[t-1] + rnorm(1,0,sqrt(sigmad2))
+  y[t] <- theta_org[t] + rnorm(1,0,sqrt(sigma2))
 }
 
 plot(y,type="l",lwd=2)
-lines(theta,lwd=2,col="red")
+lines(theta_org,lwd=2,col="red")
 
 # Gibbs sampler
 G <- 5000
 
 sigma2G <- rep(0,G)
 WG <- rep(0,G)
-thetaG <- matrix(0,nrow=G,ncol=Tbig)
+thetaG <- matrix(0,nrow=2,ncol=Tbig)
+thetaG1 <- matrix(0,nrow=G,ncol=Tbig)
+thetaG2 <- matrix(0,nrow=G,ncol=Tbig)
 
 sigma2G[1] <- 1
 WG[1] <- 1
 thetaG[1,] <- rep(0,Tbig)
+thetaG2[1,]<- rep(0,Tbig)
 
 a1 <- 0
 R1 <- 10^20
 nsig <- 0.001
 nssig <- 0.001
-nsig <- 0
-nssig <- 0
-nW  <- 6.2   # A priori W has the first 3 moments.
-nsW <- 0.42  # A a priori E[W] = 0.1
+nW <- 0.001
+nsW <- 0.001
+#nW  <- 6.2   # A priori W has the first 3 moments.
+#nsW <- 0.42  # A a priori E[W] = 0.1
 # nW  <- 0
 # nsW <- 0
 
-for(g in 2:G)
-{
-  thetaG[g,] <- theta.simulation(Tbig,y,sigma2G[g-1],WG[g-1],a1,R1)
-  sigma2G[g] <- sigma2.simulation(Tbig,y,thetaG[g,],WG[g-1],nsig,nssig)
-  WG[g] <-    W.simulation(Tbig,y,thetaG[g,],sigma2G[g],nW,nsW)
+# for(g in 2:G)
+# {
+#   thetaG <- theta.simulation(Tbig,y,sigma2G[g-1],WG[g-1],a1,R1)
+#   thetaG1[g,] <- thetaG[1,]
+#   thetaG2[g,]<- thetaG[2,]
+#   
+#   sigma2G[g] <- sigma2.simulation(Tbig,y,thetaG1[g,],WG[g-1],nsig,nssig)
+#   
+#   WG[g] <-    W.simulation(Tbig,y,thetaG1[g,],sigma2G[g],nW,nsW)
+#   WG2[g] <-    W.simulation(Tbig,y,thetaG2[g,],sigma2G[g],nW,nsW)
+# }
+V=5;
+W1=20;
+W2=2;
+
+theta1.reps <- matrix(0,nrow=G,ncol=Tbig+1)
+theta2.reps <- matrix(0,nrow=G,ncol=Tbig+1)
+V.reps <- rep(0,G)
+W1.reps <- rep(0,G)
+W2.reps <- rep(0,G)
+
+for (i in 1:G) {
+  cat(i,"\n")
+  # Sample states
+  mod <- dlmModPoly(2, dV=V, dW=c(W1,W2))
+  filt <- dlmFilter(y, mod)
+  theta <- dlmBSample(filt)
+  # Sample V and W
+  V <- drawIGpost(y-theta[1:length(y),1])
+  W1 <- drawIGpost((theta[2:Tbig,1]-theta[1:(Tbig-1),1]-theta[1:(Tbig-1),2]))
+  W2 <- drawIGpost((theta[2:Tbig,2]-theta[1:(Tbig-1),2]))
+   # V <-  sigma2.simulation(Tbig,y,theta[,1],W1,nsig,nssig)
+   # W1 <- W.simulation(Tbig,y,theta[,1],V,nW,nsW)
+   # W2 <- W.simulation(Tbig,y,theta[,2],V,nW,nsW)
+  
+  # Save iterations
+  V.reps[i] <- V
+  W1.reps[i] <- W1
+  W2.reps[i] <- W2
+  theta1.reps[i,] = theta[,1]
+  theta2.reps[i,] = theta[,2]
 }
 
+
 par(mfrow=c(2,2))
-plot(sigma2G,type="l")
-plot(WG,type="l")
-hist(sigma2G[1000:G])
-hist(WG[1000:G])
+plot(V.reps,type="l")
+plot(W1.reps,type="l")
+hist(V.reps[1000:G])
+hist(W1.reps[1000:G])
 
-mean(sigma2G[1000:G])
-var(sigma2G[1000:G])
-mean(WG[1000:G])
-var(WG[1000:G])
 
-plot(thetaG[,1],type="l")
-plot(thetaG[,10],type="l")
-plot(thetaG[,20],type="l")
-plot(thetaG[,30],type="l")
-plot(thetaG[,40],type="l")
-plot(thetaG[,50],type="l")
 
-acf(thetaG[,1])
-acf(thetaG[,10])
-acf(thetaG[,20])
-acf(thetaG[,30])
-acf(thetaG[,40])
-acf(thetaG[,50])
-acf(thetaG[,60])
-acf(thetaG[,70])
-acf(thetaG[,80])
-acf(thetaG[,90])
-acf(thetaG[,100])
+plot(theta1.reps[,1],type="l")
+plot(theta1.reps[,10],type="l")
+plot(theta1.reps[,20],type="l")
+plot(theta1.reps[,30],type="l")
+plot(theta1.reps[,40],type="l")
+plot(theta1.reps[,50],type="l")
 
-thetamean <- apply(thetaG[1000:G,],2,mean)
-thetavar <- apply(thetaG[1000:G,],2,var)
+acf(theta1.reps[,1])
+acf(theta1.reps[,10])
+acf(theta1.reps[,20])
+acf(theta1.reps[,30])
+acf(theta1.reps[,40])
+acf(theta1.reps[,50])
+acf(theta1.reps[,60])
+acf(theta1.reps[,70])
+acf(theta1.reps[,80])
+acf(theta1.reps[,90])
+acf(theta1.reps[,100])
+
+thetamean <- apply(theta1.reps[1000:G,],2,mean)
+thetavar <- apply(theta1.reps[1000:G,],2,var)
 
 ll = min(c(min(thetamean-2*sqrt(thetavar)),y))
 ul = max(c(max(thetamean+2*sqrt(thetavar)),y))
@@ -162,8 +210,9 @@ zero = rep(0,length(y))
 
 par(mfrow=c(1,1))
 plot(thetamean,lty=1,ylim=c(ll,ul),type="l",lwd=2)
-lines(theta,col="red",lwd=2)
+lines(theta_org,col="red",lwd=2)
 lines(thetamean+1.96*sqrt(thetavar),lty=2,lwd=2)
 lines(thetamean-1.96*sqrt(thetavar),lty=2,lwd=2)
 lines(zero,lty=3)
 points(y,lwd=2)
+
